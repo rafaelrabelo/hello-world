3b22b56938353009f2c1604b836444c48503e626
Rafael <rafael.rabelo@venturus.org.br>
2013-06-02 21:55:27 -0300

    bixao user says hello

--- src/br/org/venturus/github/helloworld/Main.java
+++ src/br/org/venturus/github/helloworld/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("rafaelrabelo user says hello");
+		System.out.println("bixao user says hello");
 	}
 
 }
